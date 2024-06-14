fc1b87749eec1e58fb012f7651055f4783ff57de
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2024-06-14 11:48:42 +0200

    Custom Drumbox and site responsive

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// 'use strict';
- 
+function sliderActions(audioSound, slider) {
+    const newVolume = slider.value;
+    audioSound.volume = newVolume / 100;
+}
+
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const slider = document.getElementById("slider");
+
+    slider.addEventListener("input", sliderActions(audio, slider));
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(key)
-    // console.log("Sound does not exists")
+
     if (!audio) return;
-    // console.log("Sound exists")
-    audio.currentTime = 0; // rewind to the start of audio
+    audio.currentTime = 0;
     audio.play();
     key.classList.add(`playing${e.keyCode}`)
     this.setTimeout(function() {
